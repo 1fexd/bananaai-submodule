b1c5af802bb811fbc3963a53206abea41b1e7f24
Alvaro de Santos de la Pedraja <alvarodesants@gmail.com>
2024-09-06 23:31:55 -0400

    PearAI authentication is now accessible from within pear app. Changes to Help page and Onboarding page. (#185)
    * modified onboarding page
    
    * open tutorial will navigate to "/" on logged in user
    
    * ideMessenger
    
    * Minor change to onboarding page, url to open was previously hardcoded, now it changes depending if it is being run from vscode or pear. Login to PearAI button on help page now renders  text conditionally between ReLogin to PearAi and Login to PearAI if user depending on auth status of user. open tutorial will navigate to "/" or "/onboarding" depending on auth status of user. Pear Auth should now be accessible from within the pear app.
    
    * removed eslint problems
    
    * improved handle open tutorial for better user experience
    
    * accidentally modified pearai_tutorial
    
    * Update gui/src/pages/help.tsx
    
    ---------
    
    Co-authored-by: Duke Pan <59063950+Fryingpannn@users.noreply.github.com>

--- extensions/vscode/src/ideProtocol.ts
+++ extensions/vscode/src/ideProtocol.ts
@@ -240,8 +240,7 @@ class VsCodeIde implements IDE {
       version: vscode.version,
       remoteName: vscode.env.remoteName || "local",
       extensionVersion:
-        vscode.extensions.getExtension("pearai.pearai")?.packageJSON
-          .version,
+        vscode.extensions.getExtension("pearai.pearai")?.packageJSON.version,
     });
   }
   readRangeInFile(filepath: string, range: Range): Promise<string> {
@@ -544,7 +543,7 @@ class VsCodeIde implements IDE {
     return this.getIdeSettingsSync();
   }
 
-  async getPearAuth(): Promise<PearAuth | undefined> {
+  async getPearAuth(): Promise<PearAuth> {
     const creds = await this.ideUtils.getPearCredentials();
     return creds;
   }
